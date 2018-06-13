4f62af3e95e8ee1e2a84813dff1ddc5ca214b078
Siva R <sivagrenganathan+github@protonmail.com>
2018-06-13 09:32:12 +0530

    Add endpoint for userinfo

--- wcfl/common/urls.py
+++ wcfl/common/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import sign_in, sign_out, set_token
+from .views import sign_in, sign_out, set_token, get_user_details, get_ranklist
 
 urlpatterns = [
         path('signin', sign_in, name='sign_in'),
         path('signout', sign_out, name='sign_out'),
-        path('token', set_token, name='set_token')
+        path('token', set_token, name='set_token'),
+        path('userinfo', get_user_details, name='get_user_details'),
+        path('ranklist', get_ranklist, name='get_ranklist')
         ]
