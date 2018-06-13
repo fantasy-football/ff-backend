c169ce53f1620b4ce2769ceccd38d7cdfaa455f7
Siva R <sivagrenganathan+github@protonmail.com>
2018-06-13 09:33:02 +0530

    Add view for userinfo

--- wcfl/common/views.py
+++ wcfl/common/views.py
@@ -7,8 +7,7 @@ from .decorators import set_cookies
 import requests
 import json
 
-
-@csrf_exempt
+@set_cookies
 def sign_in(request):
     if request.method == "POST":
 
@@ -21,6 +20,7 @@ def sign_in(request):
                                 status=401)
 
         try:
+
             headers = {'Authorization': 'Bearer % s' % access_token}
             r = requests.get('https://wcfl.auth0.com/userinfo',
                              headers=headers)
@@ -33,13 +33,17 @@ def sign_in(request):
                                           name=userinfo['name'],
                                           profile_picture=userinfo['picture'],
                                           email=userinfo['email'])
+
+
             else:
                 obj = User.objects.get(id=userinfo['sub'])
 
+
             request.session['user'] = obj.id
             request.session['logged_in'] = True
-
-            return JsonResponse({'success': True})
+            request.session.save()
+                
+            return JsonResponse({'Success': True})
 
         except Exception as e:
             print('Unable to fetch userinfo', e)
@@ -64,3 +68,49 @@ def sign_out(request):
         return JsonResponse({'Success': True})
     else:
         return JsonResponse({'Error': 'Invalid Request'}, status=405)
+
+
+def get_user_details(request):
+    if request.method == 'GET':
+        try:
+            if request.session['user']:
+                user = User.objects.get(id = request.session.get('user'))
+                data = {'id': user.id,
+                        'name': user.name,
+                        'pic': user.profile_picture, 
+                        'flag': user.squad_created,
+                        'score': user.score,
+                        'balance': user.balance
+                        }
+                
+                return JsonResponse({'data': data})
+
+        except Exception as e:
+            print(e, request.session['user'])
+            return JsonResponse({'Error': 'Something unexpected happened while fetching userinfo'}, status=500)
+    
+    else:
+        return JsonResponse({'Error': 'Invalid request'}, status=405)
+
+
+def get_ranklist(request):
+    if request.method == "GET":
+        try:
+            data = []
+            users = User.objects.all()
+            for rank, user in enumerate(users):
+                data.append({'rank': rank+1,
+                             'name': user.name,
+                             'pic': user.profile_picture,
+                             'score': user.score
+                             })
+            
+            return JsonResponse({'data': data})
+
+        except Exception as e:
+            print(e)
+            return JsonResponse({'Error': 'Error fetching ranklist'}, status=500)
+
+    else:
+        return JsonResponse({'Error': 'Invalid request'}, status=405)
+
