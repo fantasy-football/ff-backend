a808efe962484ebb2245742d1c99efce04899a81
Siva R <sivagrenganathan+github@protonmail.com>
2018-06-13 09:31:28 +0530

    Add all URL endpoints

--- wcfl/api/urls.py
+++ wcfl/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import players_list
+from .views import players_list, submit_squad, get_lineup
 
 urlpatterns = [
         path('players', players_list, name='players_list'),
+        path('submitSquad', submit_squad, name='submit_squad'),
+        path('lineup', get_lineup, name='get_lineup')
         ]
