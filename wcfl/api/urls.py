35825e77a0c126fcfb9329adc28cbde9d24cdc93
Siva R <sivagrenganathan+github@protonmail.com>
2018-06-24 14:09:23 +0000

    Update urls

--- wcfl/api/urls.py
+++ wcfl/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import players_list, submit_squad, get_lineup
+from .views import players_list, submit_squad, get_lineup, submit_transfer
 
 urlpatterns = [
         path('players', players_list, name='players_list'),
         path('submitSquad', submit_squad, name='submit_squad'),
-        path('lineup', get_lineup, name='get_lineup')
+        path('lineup', get_lineup, name='get_lineup'),
+	path('tranfer', submit_transfer, name='submit_transfer')
         ]
